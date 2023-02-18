cbaac28888b28ce6d3d7b59de181f3c57412d5ce
AnnaPochyla <66732418+AnnaPochyla@users.noreply.github.com>
2023-02-18 09:08:05 +0100

    Update streamlit_gpw.py

--- streamlit_gpw.py
+++ streamlit_gpw.py
@@ -6,5 +6,6 @@ url='https://drive.google.com/uc?id=' + url.split('/')[-2]
 df = pd.read_csv(url)
 
 #df=pd.read_csv('https://drive.google.com/file/d/1saiaP0TbYwiy0nap5aZrbuZL3_slWeu3/view?usp=sharing')
+streamlit.text('test')
 streamlit.text(df.size)
 streamlit.dataframe(df)
