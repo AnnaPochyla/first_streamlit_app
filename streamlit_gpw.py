99b5bed034dc310d4d83b35f48e8b91653b75181
AnnaPochyla <66732418+AnnaPochyla@users.noreply.github.com>
2023-02-18 09:18:07 +0100

    Update streamlit_gpw.py

--- streamlit_gpw.py
+++ streamlit_gpw.py
@@ -5,7 +5,7 @@ url='https://drive.google.com/file/d/1saiaP0TbYwiy0nap5aZrbuZL3_slWeu3/view?usp=
 url='https://drive.google.com/uc?id=' + url.split('/')[-2]
 streamlit.title('My Parents New Healthy Diner')
 streamlit.text('test')
-df = pd.read_csv(url)
+df = pd.read_csv(url,usecols=['Nazwa','Waluta','Kurs otwarcia','Kurs maksymalny','Kurs minimalny','Kurs zamknięcia','Zmiana kursu %','Wartość obrotu (w tys.)','Data'])
 
 #df=pd.read_csv('https://drive.google.com/file/d/1saiaP0TbYwiy0nap5aZrbuZL3_slWeu3/view?usp=sharing')
 
