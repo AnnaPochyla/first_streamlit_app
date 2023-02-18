c74fbff310afa849dd891869199c53db45a845f7
AnnaPochyla <66732418+AnnaPochyla@users.noreply.github.com>
2023-02-18 09:14:36 +0100

    Update streamlit_gpw.py

--- streamlit_gpw.py
+++ streamlit_gpw.py
@@ -5,9 +5,9 @@ url='https://drive.google.com/file/d/1saiaP0TbYwiy0nap5aZrbuZL3_slWeu3/view?usp=
 url='https://drive.google.com/uc?id=' + url.split('/')[-2]
 streamlit.title('My Parents New Healthy Diner')
 streamlit.text('test')
-#df = pd.read_csv(url)
+df = pd.read_csv(url)
 
 #df=pd.read_csv('https://drive.google.com/file/d/1saiaP0TbYwiy0nap5aZrbuZL3_slWeu3/view?usp=sharing')
 
-#streamlit.text(df.size)
-#streamlit.dataframe(df)
+streamlit.text(df.size)
+streamlit.dataframe(df)
