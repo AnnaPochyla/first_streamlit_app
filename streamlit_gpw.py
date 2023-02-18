0c2104234a3fbe5a5dc2601751efb92cf75eeacc
AnnaPochyla <66732418+AnnaPochyla@users.noreply.github.com>
2023-02-18 09:09:01 +0100

    Update streamlit_gpw.py

--- streamlit_gpw.py
+++ streamlit_gpw.py
@@ -3,9 +3,10 @@ import pandas as pd
 import streamlit
 url='https://drive.google.com/file/d/1saiaP0TbYwiy0nap5aZrbuZL3_slWeu3/view?usp=sharing'
 url='https://drive.google.com/uc?id=' + url.split('/')[-2]
+streamlit.text('test')
 df = pd.read_csv(url)
 
 #df=pd.read_csv('https://drive.google.com/file/d/1saiaP0TbYwiy0nap5aZrbuZL3_slWeu3/view?usp=sharing')
-streamlit.text('test')
+
 streamlit.text(df.size)
 streamlit.dataframe(df)
